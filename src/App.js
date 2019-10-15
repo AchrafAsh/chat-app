5ad59abc03be2cffacd1e7d7a80d8222226a17a7
AchrafAsh <aitsidihammou.achraf@gmail.com>
2019-10-15 08:33:34 +0200

    I have removed my key api and other personal IDs

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import RoomList from "./components/RoomList";
 import SendMessageForm from "./components/SendMessageForm";
 import NewRoomForm from "./components/NewRoomForm";
 import "./style.css";
-
+import * as constants from "./constants";
 import {
   TokenProvider,
   ChatManager
 } from "@pusher/chatkit-client/react-native";
 
 const userId = "perborgen";
-// const chatkitKeyApi = "282a4ee1-ea6f-428c-8095-a49ee783f478:MWgL6ngwT7tc3iNMzXBr/QuTzTV9EXwM72qntxsHo1U=";
-const instanceLocator = "v1:us1:37bcdde8-4220-43fd-b301-7bbca64f2d15";
-const tokenUrl =
-  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/37bcdde8-4220-43fd-b301-7bbca64f2d15/token";
+const chatkitKeyApi = constants.KEY_API;
+const instanceLocator = constants.INSTANCELOCATOR;
+const tokenUrl = constants.TOKEN_URL;
 
 class App extends Component {
   constructor() {
@@ -49,7 +48,7 @@ class App extends Component {
           })
           .catch(err => console.log("error on joinable rooms", err));
         currentUser.subscribeToRoomMultipart({
-          roomId: "f9086dd7-7544-43b9-8d5d-a38298d3c742",
+          roomId: constants.ROOM_ID,
           hooks: {
             onMessage: message => {
               this.setState({
@@ -65,7 +64,7 @@ class App extends Component {
   sendMessage = text => {
     this.currentUser.sendMessage({
       text: text,
-      roomId: "f9086dd7-7544-43b9-8d5d-a38298d3c742"
+      roomId: constants.ROOM_ID
     });
   };
 
